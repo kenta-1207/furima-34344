bb1bd3af21e398e9e42ae561304ac238d62a7afc
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-04-20 22:31:41 +0900

    バリデーションの再定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,14 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name_ruby, presence: true
   validates :first_name_ruby, presence: true
-  
+  validates :birthday, presence: true
+  validates :password,confirmation: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX
+with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/} do
+  validates :first_name
+  validates :last_name
+  validates :first_name_ruby
+  validates :last_name_ruby
+end
 end
