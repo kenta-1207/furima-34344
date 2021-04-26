c7bce93c3aafe864453343e105e8aa07fbaa89e7
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-04-26 21:29:17 +0900

    テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_ruby, presence: true
-  validates :first_name_ruby, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } 
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } 
+  validates :last_name_ruby, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_ruby, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
   validates :password,confirmation: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX
-with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/} do
-  validates :first_name
-  validates :last_name
-  validates :first_name_ruby
-  validates :last_name_ruby
-end
+  validates_format_of :password, with: PASSWORD_REGEX
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/} do
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(numbeer: 2)}
+    nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name             {'あああ'}
     first_name            {'いいい'}
-    last_name_ruby        {'ううう'}
-    first_name_ruby       {'えええ'}
+    last_name_ruby        {'ウウウ'}
+    first_name_ruby       {'エエエ'}
     birthday              {'19801111'}
   end
 end
