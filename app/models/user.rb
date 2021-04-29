548f1f5bbc9948c9109337dd78f75d3379bc9329
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-04-29 21:16:28 +0900

    バリデーションにてwith_optionに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } 
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } 
-  validates :last_name_ruby, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_ruby, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-  validates :password,confirmation: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/ } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_ruby
+      validates :first_name_ruby
+    end
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: {with: PASSWORD_REGEX}, allow_blank: true
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/} do
-  end
 end
