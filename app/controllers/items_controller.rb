062ec5434d9989e88dffcf1553bafcb3201918fe
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-05-01 19:27:18 +0900

    ログインユーザーのみ出品可能に設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
 
   def index
   end
-
-  def new
-    @item = Item.new
-  end
   
+  def new
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
