c624b1b5fc8ab1a38156b114781c0a5036603574
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-05-08 16:22:31 +0900

    reference型userの追記

--- db/migrate/20210501090306_create_items.rb
+++ db/migrate/20210501090306_create_items.rb
@@ -1,14 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :description
-      t.integer :category_id
-      t.integer :item_status_id
-      t.integer :delivery_charge_id
-      t.integer :prefecture_id
-      t.integer :shipping_day_id
-      t.integer :price
+      t.string :name,                null: false
+      t.text :description,           null: false
+      t.integer :category_id,        null: false
+      t.integer :item_status_id,     null: false
+      t.integer :delivery_charge_id, null: false
+      t.integer :prefecture_id,      null: false
+      t.integer :shipping_day_id,    null: false
+      t.integer :price,              null: false
+      t.references :user,            forein_key: true
       t.timestamps
     end
   end
