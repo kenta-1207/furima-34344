8cb5d6663fb2af64b9eeb5cac531023352a412ce
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-05-08 16:20:17 +0900

    Item_modelバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,27 @@
 class Item < ApplicationRecord
-  with_options presence: true do
-    validates :name
-    validates :description
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :item_status_id
     validates :delivery_charge_id
     validates :prefecture_id
     validates :shipping_day_id
-    validates :items_image
   end
 
-  validates :price, presence: true format: {/\A[-]?[0-9]+(\.[0-9]+)?\z/}
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}, allow_blank: true
+  validates :price, presence: true
+  
+  validates :image, presence: true
 end
+
