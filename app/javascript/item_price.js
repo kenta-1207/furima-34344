48933626aa44d214a3a4695dd21112f7f0a266f5
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-05-11 19:47:48 +0900

    一覧表示を新規投稿順に変更

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,10 @@
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
+  const inputValue = priceInput.Value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
 
-const addTaxDom = document.getElementById("add-tax-price");
-addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-
-  const profitValue = document.getElementById("profit")
+  const profitValue = document.getElementById("profit");
   profitValue.innerHTML = (Math.floor(inputValue * 0.9));
-})
 });
+;
